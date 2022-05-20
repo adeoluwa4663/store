87fe83f49daab0f70d17ff687618fdcc5af61fb5
adeoluwa4663 <74413929+adeoluwa4663@users.noreply.github.com>
2022-05-20 20:07:27 +0100

    Add files via upload

---
+++ Pblic/JS/home.js
@@ -0,0 +1,15 @@
+const productContainers = [...document.querySelectorAll(".product-container")];
+const nextBtn = [...document.querySelectorAll(".next-btn")];
+const preBtn = [...document.querySelectorAll(".pre-btn")];
+
+productContainers.forEach((item, i) => {
+  let containerDimension = item.getBoundingClientRect();
+  let containerWidth = containerDimension.width;
+
+  nextBtn[i].addEventListener("click", () => {
+    item.scrollleft += containerWidth;
+  });
+  preBtn[i].addEventListener("click", () => {
+    item.scrollLeft -= containerWidth;
+  });
+});
